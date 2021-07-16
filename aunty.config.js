4726cf320548282344cc39bf6ab7cae2aa6ce8bd
Colin Gourlay <gourlay.colin@abc.net.au>
2021-07-16 10:23:12 +1000

    Add aunty config to create a secondary deploy to always access the latest release on the same URL (which we must cache-bust)

--- aunty.config.js
+++ aunty.config.js
@@ -1,4 +1,11 @@
+const fs = require('fs');
 const path = require('path');
+
+const DEPLOY_TO = `/www/res/sites/news-projects/<name>/<id>`;
+const PUBLIC_PATH = path.join(__dirname, 'public');
+const REDIRECT_DIR = `.redirect`;
+const REDIRECT_PATH = path.join(__dirname, REDIRECT_DIR);
+const TEMPLATE_FILENAME = 'index.html';
 
 module.exports = {
   type: 'svelte',
@@ -6,6 +13,30 @@ module.exports = {
     extractCSS: true,
     includedDependencies: [/carbon-/]
   },
+  deploy: [
+    {
+      to: DEPLOY_TO
+    },
+    config => {
+      const [, nonReleaseIdVariant] = config.id.match(/(?!\-)([a-z\-]+)/) || []; // e.g. master, pre, alpha, beta
+      const redirectId = `latest${nonReleaseIdVariant ? `-${nonReleaseIdVariant}` : ''}`;
+      const relativeAssetURL = `../${config.id}/`;
+      const publicIndexHTML = fs.readFileSync(path.join(PUBLIC_PATH, 'index.html'), { encoding: 'utf8' });
+      const redirectIndexHTML = publicIndexHTML.replace(/(index\.)/g, `${relativeAssetURL}$1`);
+
+      if (!fs.existsSync(REDIRECT_PATH)) {
+        fs.mkdirSync(REDIRECT_PATH);
+      }
+
+      fs.writeFileSync(path.join(REDIRECT_PATH, 'index.html'), redirectIndexHTML);
+
+      return {
+        ...config,
+        from: REDIRECT_DIR,
+        to: DEPLOY_TO.replace('<id>', redirectId)
+      };
+    }
+  ],
   webpack: config => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
