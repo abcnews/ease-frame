1794710f56eeb02b26cfd84ddcd711c3e28b8ef7
Colin Gourlay <gourlay.colin@abc.net.au>
2021-07-21 13:45:42 +1000

    Assure TS compiler we have a target element to mount to

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import './global.scss';
 import App from './components/App/App.svelte';
 
-new App({
-  target: document.getElementById('app'),
-  props: {
-    // debugTextInputValue: 'https://www.abc.net.au/news/2021-07-15/13446364'
-  }
-});
+const target = document.getElementById('app');
+
+if (target) {
+  new App({
+    target,
+    props: {
+      // debugTextInputValue: 'https://www.abc.net.au/news/2021-07-15/13446364'
+    }
+  });
+}
