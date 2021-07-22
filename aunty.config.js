ab75bcf1e9c1f0873275df114b38b1b5bdd8f1d4
Colin Gourlay <gourlay.colin@abc.net.au>
2021-07-23 08:38:43 +1000

    Update webpack config to disable SASS and Svelte warnings in dependencies

--- aunty.config.js
+++ aunty.config.js
@@ -9,11 +9,17 @@ const REDIRECT_DIR = `.redirect`;
 const REDIRECT_PATH = path.join(__dirname, REDIRECT_DIR);
 const TEMPLATE_FILENAME = 'index.html';
 
+const includedDependencies = [/carbon-/];
+const getLoaderDefinition = (config, testSourceMatch, loaderMatch) =>
+  config.module.rules
+    .find(({ test }) => test.source.indexOf(testSourceMatch) > -1)
+    .use.find(({ loader }) => loader.indexOf(loaderMatch || testSourceMatch) > -1);
+
 module.exports = {
   type: 'svelte',
   build: {
     extractCSS: true,
-    includedDependencies: [/carbon-/]
+    includedDependencies
   },
   deploy: [
     {
@@ -42,9 +48,24 @@ module.exports = {
     }
   ],
   webpack: config => {
+    // De-dupe svelte
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       svelte: path.resolve('node_modules', 'svelte')
+    };
+
+    // Disable dart-sass warnings
+    getLoaderDefinition(config, 'scss', 'sass').options = { sassOptions: { quietDeps: true } };
+
+    // Disable svelte warnings when compiling dependencies
+    getLoaderDefinition(config, 'svelte').options.onwarn = (warning, handler) => {
+      for (const pattern of includedDependencies) {
+        if (pattern.test(warning.filename)) {
+          return;
+        }
+      }
+
+      handler(warning);
     };
 
     return config;
